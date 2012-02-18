7ce976a27743bae824170f468ebce382269c1c4f
unknown <Matt@Matt-PC.(none)>
2012-02-17 22:22:36 -0500

    outputting first page of subscriptions w title

--- youtube.rb
+++ youtube.rb
@@ -5,22 +5,42 @@ require 'nokogiri'
 get '/user/:username' do
   doc = feed params[:username]
 
-  output = ''
+  videos = Video.parse_from_feed(doc)
 
-  doc.xpath("//a:entry", {"a" => "http://www.w3.org/2005/Atom"}).each do |entry|
-    title = entry.xpath("a:title", {"a" => "http://www.w3.org/2005/Atom"}).first
-    content = entry.xpath("a:content", {"a" => "http://www.w3.org/2005/Atom"}).first
-    video_id = ''
-    if /watch\?v=([A-Za-z0-9_-]+)/ =~ content
-      video_id = $1
-    end
-    output << "<div>#{title.text.to_s} <img src='http://#{video_id}' /></div>"
-  end
-
-  output
+  erb :index, :locals => { :videos => videos }
 end
 
 def feed(username)
   url = "http://gdata.youtube.com/feeds/base/users/#{username}/newsubscriptionvideos"
   Nokogiri::XML(open(url)) 
 end
+
+class Video
+
+  attr_accessor :title, :video_id
+
+  def self.parse_from_feed(doc)
+    videos = Array.new 
+
+    doc.xpath("//a:entry", {"a" => "http://www.w3.org/2005/Atom"}).each do |entry|
+      videos << Video.new(entry)
+    end
+
+    videos
+  end
+
+  def initialize(node)
+    @title = atom_xpath(node, "title").first.text.to_s
+    
+    content = atom_xpath(node, "content").first 
+    @video_id = $1 if /watch\?v=([A-Za-z0-9_-]+)/ =~ content
+  end
+
+  private
+
+  def atom_xpath(entry, name)
+    entry.xpath("a:#{name}", {"a" => "http://www.w3.org/2005/Atom"})
+  end
+
+end
+
